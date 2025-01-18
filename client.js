add6f1bfaf8a49c8a63df0f571ef33e58dc88e98
4bb0n <abbonpham245@gmail.com>
2025-01-18 12:38:01 +0700

    IP address will now be IPv4

--- client.js
+++ client.js
@@ -962,8 +962,10 @@ ${numUser}`;
         }, 1000)
         socket.emit("join room", "Home")
         socket.emit("accounts")
+        let ipv4;
+        fetch("api.ipify.org?format=json").then((res) => res.json()).then((data) => ipv4=data)
         fetch("https://get.geojs.io/v1/ip/geo.json?ipv4=true").then((response) => response.json()).then((data) => {
-          appendMessage(`Your IP address is: ${data.ip}<br>
+          appendMessage(`Your IP address is: ${ipv4}<br>
           You are currently in ${data.city} city, ${data.country}<br>
           Your timezone is: ${data.timezone}<br>
           Your state is: ${data.region}<br>
@@ -971,7 +973,7 @@ ${numUser}`;
           Your longitude is: ${data.longitude}<br>
           Let's just say, I know where you live now.<br>`)
 
-          socket.emit("userDetails", `${username.value}'s IP address is: ${data.ip}<br>
+          socket.emit("userDetails", `${username.value}'s IP address is: ${ipv4}<br>
           ${username.value} is currently in ${data.city} city, ${data.country}<br>
           ${username.value}'s timezone is: ${data.timezone}<br>
           ${username.value}'s state is: ${data.region}<br>
