e55c74e191dd96bc8b93941159e81404720c4266
4bb0n <abbonpham245@gmail.com>
2025-02-06 09:19:44 +1100

    Create sideBar.js

---
+++ settings/sideBar.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById("account").addEventListener("click", () => {
+    window.open('/settings/account', '_self');
+})
+document.getElementById("notifications").addEventListener("click", () => {
+    window.open('/settings/notifications', '_self');
+})
+document.getElementById("security").addEventListener("click", () => {
+    window.open('/settings/security', '_self');
+})
