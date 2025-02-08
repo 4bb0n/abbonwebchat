2c2f46e73152938ed99d048be382a72547f64934
4bb0n <abbonpham245@gmail.com>
2025-02-08 17:02:32 +1100

    Update sideBar.js

--- settings/sideBar.js
+++ settings/sideBar.js
@@ -9,3 +9,9 @@ document.getElementById("notifications").addEventListener("click", () => {
 document.getElementById("security").addEventListener("click", () => {
     window.open('/settings/security', '_self');
 })
+document.getElementById("home").addEventListener("click", () => {
+    window.open('/', '_self');
+})
+document.getElementById("personalization").addEventListener("click", () => {
+    window.open('/settings/personalization', '_self');
+})
